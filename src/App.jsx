7a2e7adafa8adb8d52b751e42fc2b5bdbd173bc5
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-29 23:20:19 +0100

    align

--- src/App.jsx
+++ src/App.jsx
@@ -53,13 +53,24 @@ const getCities = async () => {
   };
   
   const getHouseId = async (id) => {
-  try {
-    const res = await fetch(`http://localhost:3001/city/${id}`);
-    if (res.ok) {
-      const data = await res.json();
-      console.log(data);
-      setCity(data);
-      console.log(city)
+    
+    try {
+      if (id) {
+        const res = await fetch(`http://localhost:3001/city/${id}`);
+        if (res.ok) {
+          const data = await res.json();
+          console.log(data);
+          setCity(data);
+          console.log(city)
+        }
+      } else {
+                const res = await fetch("http://localhost:3001/city");
+        if (res.ok) {
+          const data = await res.json();
+          console.log(data);
+          setCity(data);
+          
+        }
       }
   } catch (error) {
     console.log("server error");
@@ -67,62 +78,40 @@ const getCities = async () => {
 };
 
 useEffect(()=>{
-  getCities()
+  getHouseId()
+  // getCities()
 }, [])
      
-  
+  useEffect(()=>{},[city])
   return (
     <>
-      <h1>Welcome in Clone<span style={{color:"white"}}>Air</span>BnB</h1>
+      <h1>Welcome in Clone<span style={{ color: "white" }}>Air</span>BnB</h1>
+      <Button>Become a Host</Button>
       <Container>
-        <Row>
-          <Col xs={12} md={6} className="form_input">
-
-<InputGroup className="mb-3">
-    <InputGroup.Prepend>
-      <Button variant="outline-secondary">Button</Button>
-    </InputGroup.Prepend>
-    <FormControl aria-describedby="basic-addon1" placeholder="Search for a city"
-            className="mr-sm-2 form_input"/>
-  </InputGroup>
-{/* <Navbar className="bg-light justify-content-between">
-    <Form inline className="form_input">
-    <Form.Control 
-            type="text"
-                  // onChange={(e) => getHouseId(e.target.value)}
-                    // setSelected(e.target.value)}
-            //value={cities}
-            placeholder="Search for a city"
-            className="mr-sm-2 form_input" />
-    <Button type="submit" onClick={(e)=>console.log(e.target.value)}>search</Button>
-  </Form>
-      </Navbar> */}
-          </Col>
-
-
-          {/* <Col xs={12} md={6}>
-<Navbar className="bg-light justify-content-between">
-    <Form inline>
-    <Form.Control 
-            type="date"
-            onChange={(e)=>setDate(e.target.value)}
-            value={date}
-            placeholder="Search for a date"
-            className=" mr-sm-2 form_input"/>
-    <Button type="submit" onClick={(e)=>setDate(e.target.value)}>search</Button>
-  </Form>
-      </Navbar>
-          </Col> */}
+        <Row className="d-flex">
+          <Col xs={12} md={9} className="xs-mt-3">
+            <Row>
+            {
+              city.map(e => e.Houses.map(h => (
+              <Col md={4}>
+                  <Card >
+                    <Card.Body>
+                      <Card.Title>{ h.title}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                        <Card.Text>{ h.description}</Card.Text>
+                        <Card.Text>rate { h.rate}</Card.Text>
+                        <Card.Link href="#">Show more</Card.Link>
+                        <Card.Link href="#">Another Link</Card.Link>
+                    </Card.Body>
+                  </Card>
+               </Col>)
+              ))
+            }
+            </Row>
+            </Col>
         </Row>
-      </Container>
-      <Row>
-        <Col md={6}>
-          <h2>Select a House</h2>
-        </Col>
-      </Row>
-
-      
-      </>
+   </Container>
+    </>
   );
 }
 
